6afb96ef05fbec5d609e7c16cd70e42e2fa59492
AnShatylo <a.shatilo18@gmail.com>
2024-10-08 00:00:12 +0200

    V1

---
+++ Typescript homework 1/src/generics/5.ts
@@ -0,0 +1,15 @@
+export enum UserRole {
+  admin = "admin",
+  editor = "editor",
+  guest = "guest",
+}
+
+type RoleDescription = Record<UserRole, string>;
+
+// Замініть наступний код на версію за допомогою Record
+const descrRole: RoleDescription = {
+  admin: "Admin User",
+  editor: "Editor User",
+  guest: "Guest User",
+};
+
